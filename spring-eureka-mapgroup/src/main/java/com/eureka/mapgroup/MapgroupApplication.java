0eb3072e594bab2dfa4b717eaf60e750d496cace
Chrs Boyd <chrisboyd360@gmail.com>
2020-11-08 19:50:46 -0800

    base spring initiliazr

---
+++ spring-eureka-mapgroup/src/main/java/com/eureka/mapgroup/MapgroupApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.mapgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapgroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapgroupApplication.class, args);
+	}
+
+}
