8d98e0b5c97da43b2afea549468d51c430d6666a
Chrs Boyd <chrisboyd360@gmail.com>
2020-11-09 19:40:40 -0800

    pre-test for messages working
    Added @ResponseStatus(httpresponse) to each exception to return 404

--- spring-eureka-map/src/main/java/com/eureka/map/controllers/MapNotFoundException.java
+++ spring-eureka-map/src/main/java/com/eureka/map/controllers/MapNotFoundException.java
@@ -1,5 +1,9 @@
 package com.eureka.map.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class MapNotFoundException extends RuntimeException {
 	MapNotFoundException(int id){
 		super("Could not find map " + id);
--- spring-eureka-user/src/main/java/com/eureka/user/controllers/UserNotFoundException.java
+++ spring-eureka-user/src/main/java/com/eureka/user/controllers/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.eureka.user.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class UserNotFoundException extends RuntimeException {
 	UserNotFoundException(int id){
 		super("Could not find user " + id);
