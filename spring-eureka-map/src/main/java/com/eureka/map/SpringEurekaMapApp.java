fe21fa906070635193bea9e9ba60b32a127e4a84
Chrs Boyd <chrisboyd360@gmail.com>
2020-11-11 13:40:59 -0800

    remove eureka user, map and mapgroup
    was causing issues running in Docker

--- spring-eureka-map/src/main/java/com/eureka/map/SpringEurekaMapApp.java
+++ spring-eureka-map/src/main/java/com/eureka/map/SpringEurekaMapApp.java
@@ -2,8 +2,6 @@ package com.eureka.map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -22,7 +20,6 @@ class RestTemplateConfig {
 	
 	// Create a bean for restTemplate to call services
 	@Bean
-	@LoadBalanced		// Load balance between service instances running at different ports.
 	public RestTemplate restTemplate() {
 	    return new RestTemplate();
 	}
